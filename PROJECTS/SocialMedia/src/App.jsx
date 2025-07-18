decbbed5deb8d9ee42ae216b1cf67c44e05b7d67
rahulshrivastwa99 <rahulshrivastwa1@gmail.com>
2025-07-18 22:59:15 +0530

    Added header & footer with bootstrap

--- PROJECTS/SocialMedia/src/App.jsx
+++ PROJECTS/SocialMedia/src/App.jsx
@@ -1,9 +1,15 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <>
-      <div>Placeholder</div>
+      <Header></Header>
+      <Sidebar></Sidebar>
+      <Footer></Footer>
     </>
   );
 }
