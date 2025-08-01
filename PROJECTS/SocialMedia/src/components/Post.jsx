6ffedf489671d1d36f7028fa63423cc6343c3709
rahulshrivastwa99 <rahulshrivastwa1@gmail.com>
2025-08-01 21:03:55 +0530

    added delete from react-icons

--- PROJECTS/SocialMedia/src/components/Post.jsx
+++ PROJECTS/SocialMedia/src/components/Post.jsx
@@ -1,3 +1,5 @@
+import { AiFillDelete } from "react-icons/ai";
+
 const Post = ({ post }) => {
   return (
     <div className="card post-card" style={{ width: "18rem" }}>
@@ -5,8 +7,8 @@ const Post = ({ post }) => {
         <h5 className="card-title">
           {post.title}
           <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-            {post.reactions}
-            <span class="visually-hidden">unread messages</span>
+            {/* {post.reactions} */}
+            <AiFillDelete />
           </span>
         </h5>
         <p className="card-text">{post.content}</p>
